be94e4b0ec51986846f99b2cf7cb8b839b580a08
40423232 <40423232@gm.nfu.edu.tw>
2016-01-16 01:29:14 +0800

    40423232

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -27,7 +27,7 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),('2015作業主頁', '../../'),
-         ('40423232 倉儲','http://40423232.github.io/cpbtest2/'))
+         ('40423232 個人倉儲','http://40423232.github.io/cpbtest2/'))
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
